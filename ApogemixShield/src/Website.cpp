a1ea17d654d0d200705a0dcafb65949330340ce1
Michał Kowalski <248959@student.pwr.edu.pl>
2022-08-29 21:56:12 +0200

    Ustawienia lory przez WiFi

--- ApogemixShield/src/Website.cpp
+++ ApogemixShield/src/Website.cpp
@@ -17,8 +17,14 @@ void Website::start() {
         request->send(200, "text/html", generateHtml());
     });
 
+    server.on("/debug", HTTP_GET, [this](AsyncWebServerRequest *request) {
+
+        request->send(200, "text/html", glob.dataFrame.toString());
+    });
+
     server.on("/wifioff", HTTP_GET, [this](AsyncWebServerRequest *request) {
 
+        request->send(200, "text/html", String("<meta http-equiv=\"refresh\" content=\"0; URL=/\" />"));
         stop();
     });
 
@@ -50,11 +56,16 @@ String Website::generateHtml() {
     html += "<!DOCTYPE html><head>    <title>Apogemix mini configuration page</title></head><body>    <h3>        Apogemix mini    </h3><hr>    <p>        Current settings:        <ul>            <li>Apogee ignite delay: <strong>";
     
     html += String(glob.memory.firstSeparDelay_ms);
-    html += "</strong> ms,</li>            <li>Second parachute ignition altitude: <strong>";
+    html += "</strong> ms,</li> <li>Second parachute ignition altitude: <strong>";
     html += String(glob.memory.secondSeparAltitude);
-    html += "</strong> m,</li>            <li>Wifi active time: <strong>";
+    html += "</strong> m,</li> <li>Wifi active time: <strong>";
     html += String(glob.memory.wifiActiveTime_min);
-    html += "</strong> min,</li>        </ul>    </p><hr>    <a href='/FlightData.apg'>Download data from last flight.</a>    <p>        Flight data table:        <table border='1'>            <tr>                <th>Num</th>                <th>Apogee [m]</th>                <th>Max speed [m/s]</th>            </tr>";
+    html += "</strong> min,</li> <li>LoRa frequency: <strong>";
+    html += String(glob.memory.loraFreqMHz);
+    html += "</strong> MHz,</li> <li>LoRa send data period: <strong>";
+    html += String(glob.memory.loraDelay_ms);
+    html += "</strong> ms.</li>";
+    html += "</ul>    </p><hr>    <a href='/FlightData.apg'>Download data from last flight.</a>    <p>        Flight data table:        <table border='1'>            <tr>                <th>Num</th>                <th>Apogee [m]</th>                <th>Max speed [m/s]</th>            </tr>";
     
     for (uint8_t i = 0; i < FLIGHTS_IN_MEM; i++) {
 
@@ -63,7 +74,10 @@ String Website::generateHtml() {
         html += "<td>" + String(glob.memory.flight[i].maxSpeed) + "</td></tr>";
     }
     
-    html += "</table>    </p><hr>    <p>        Change settings:        <form>            <label for='set1'>Apogee ignite delay:</label><br>            <input type='number' min='1' max='5000' id='set1' name='set1'><br>            <label for='set2'>Second parachute ignition altitude:</label><br>            <input type='number' min='50' max='5000' id='set2' name='set2'><br>            <label for='set3'>Wifi active time:</label><br>            <input type='number' min='2' max='15' id='set3' name='set3'><br>            <br><input type='submit'><hr>        </form>    </p>    </p>    <a href='/wifioff'>Turn off the wifi.</a>    <p><hr>    </body></html>";
+    html += "</table>    </p><hr>    <p>        Change settings:        <form>            <label for='set1'>Apogee ignite delay:</label><br>            <input type='number' min='1' max='5000' id='set1' name='set1'><br>            <label for='set2'>Second parachute ignition altitude:</label><br>            <input type='number' min='50' max='5000' id='set2' name='set2'><br>            <label for='set3'>Wifi active time:</label><br>            <input type='number' min='2' max='15' id='set3' name='set3'><br>";
+    html += "<label for='set4'>LoRa frequency:</label><br> <input type='number' min='100' max='999' id='set4' name='set4'><br>";
+    html += "<label for='set5'>LoRa send period:</label><br> <input type='number' min='500' max='5000' id='set5' name='set5'><br>";
+    html +="<br><input type='submit'><hr>        </form>    </p>    </p>    <a href='/wifioff'>Turn off the wifi.</a>    <p><hr>    </body></html>";
 
     return html;
 }
@@ -74,13 +88,17 @@ void Website::handleArgs(AsyncWebServerRequest *request) {
 
     if (request->hasParam("set1") && request->hasParam("set2") && request->hasParam("set3")) {
 
-        uint8_t set1 = request->getParam("set1")->value().toInt();
-        uint8_t set2 = request->getParam("set2")->value().toInt();
+        uint16_t set1 = request->getParam("set1")->value().toInt();
+        uint16_t set2 = request->getParam("set2")->value().toInt();
         uint8_t set3 = request->getParam("set3")->value().toInt();
+        uint16_t set4 = request->getParam("set4")->value().toInt();
+        uint16_t set5 = request->getParam("set5")->value().toInt();
 
         if (set1 >= 1 && set1 <= 5000) glob.memory.firstSeparDelay_ms   = set1;
         if (set2 >= 50 && set2 <= 5000) glob.memory.secondSeparAltitude = set2;
         if (set3 >= 2 && set3 <= 15) glob.memory.wifiActiveTime_min     = set3;
+        if (set4 >= 100 && set4 <= 999) glob.memory.loraFreqMHz         = set4;
+        if (set5 >= 500 && set3 <= 5000) glob.memory.loraDelay_ms       = set5;
 
         EEPROM.put(0, glob.memory);
         EEPROM.commit();
